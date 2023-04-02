bc67c4ae665d889f0fdd6e43b04f9296357e4d05
nemanja <snowfoot.mc@gmail.com>
2023-04-02 16:49:23 +0200

    UI

--- script.js
+++ script.js
@@ -1,68 +1,109 @@
 function getComputerChoice(){
-let choice;
-const RPS = Math.random();
-if (RPS<0.34)
-choice = "rock";
-else if (RPS<0.67)
-choice = "paper";
-else 
-choice = "scissors";
-return choice;
+    let choice;
+    const RPS = Math.random();
+    if (RPS<0.34){
+        choice = "rock";
+        }
+    else if (RPS<0.67){
+        choice = "paper";
+        }
+    else{ 
+        choice = "scissors";
+        }
+    return choice;
 }
 
- function playRound(player, computer){
-if (player == computer)
-console.log("It's a draw")
-else if (player == "rock"){
+function playRound(player, computer){
+    if (player == computer){
+        console.log("It's a draw")
+        }
+    else if (player == "rock"){
         if (computer == "paper"){
             console.log("You lose. Computer wins!")
-        return 1;}
-            else {console.log("You win!")
-            return 2;}
+            return 1;
+        }
+        else {
+            console.log("You win!")
+            return 2;
+        }
     }
-else if (player == "paper"){
-        if (computer == "scissors"){
-        console.log("You lose. Computer wins!")
-        return 1;}
-        else {console.log("You win!")
-        return 2;}
-    }
-else if (player == "scissors"){
+    else if (player == "paper"){
+            if (computer == "scissors"){
+                console.log("You lose. Computer wins!")
+                return 1;}
+            else {
+                console.log("You win!")
+                return 2;
+                }
+        }
+    else if (player == "scissors"){
         if (computer == "rock"){
-        console.log("You lose. Computer wins!")
-        return 1;}
+            console.log("You lose. Computer wins!")
+            return 1;}
         else {console.log("You win!")
-        return 2;}
+            return 2;}
+        }
+}
+
+
+
+function game(playerChoice){
+    let computerChoice = getComputerChoice();
+    let result = playRound(playerChoice,computerChoice);
+    console.log("Your choice: " + playerChoice + " | Computers choice: " + computerChoice)
+    return result;
+}
+
+
+var pWins = 0, cWins = 0;
+let winner;
+
+
+function getWinner(winner){
+    if (winner == 1){
+        cWins++;
+    } else if(winner == 2){
+        pWins++;
     }
-    else console.log("Your input was inorrect!!!")
+    console.log(cWins)
+    console.log(pWins)
+    const scoreText = document.querySelector('#score');
+    scoreText.innerHTML = "Player wins:" + pWins + " | Computer wins:" + cWins;
+    if (cWins == 5|| pWins == 5){
+        if (cWins == 5){
+            alert("Computer is winner with " + cWins + " wins!")
+        }
+        else {
+            alert("Player is winner with " + pWins + " wins!")
+        }
+        let playAgain = prompt("Do you want to play again ?(yes/no");
+        playAgain = playAgain.toLocaleLowerCase();
+        if (playAgain == "yes"){
+            cWins = 0;
+            pWins = 0;
+        }
+        
+        alert(playAgain);
+        
+    }
 }
 
 
-
-function game(){
-let winNumber = prompt("Best of how many games ?");
-winNumber = winNumber/2 -winNumber%2 + 1;
-
-for (let i = 0,j = 0, k = 1;i<winNumber && j <winNumber;k++){
-console.log("Round " + k + ":")
-let playerChoice = prompt("Rock, paper or scissors ?");
-playerChoice = playerChoice.toLowerCase();
-let computerChoice = getComputerChoice();
-
-
-let result = playRound(playerChoice,computerChoice);
-if (result==1)
-i++;
-else if (result == 2)
-j++;
-
-console.log("Your choice: " + playerChoice + " | Computers choice: " + computerChoice)
-console.log("Player wins:" + j + " | Computer wins:" + i)
-if (i >= winNumber || j >= winNumber)
-if (i >= winNumber)
-console.log("\nComputer is winner with " + i + " wins.")
-else console.log("\nYou are winner with " + j + " wins.")
-}
-}
-game();
- 
+    const rockPlayer = document.querySelector('#rock');
+    rockPlayer.addEventListener('click',function(e){
+        winner = game("rock");
+        getWinner(winner);
+        
+    })
+    
+    const paperPlayer = document.querySelector('#paper');
+    paperPlayer.addEventListener('click',function(e){
+        winner = game("paper");
+        getWinner(winner);
+    })
+    
+    const scissorsPlayer = document.querySelector('#scissors');
+    scissorsPlayer.addEventListener('click',function(e){
+        winner = game("scissors");
+        getWinner(winner);
+    })
